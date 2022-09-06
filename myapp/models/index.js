fef0a5d4e00c84c761b9dda0f8f93b97de0022e7
Aiswarya Sasidharan <102617444+aish-26-2000@users.noreply.github.com>
2022-09-06 10:15:17 +0530

    Update index.js

--- myapp/models/index.js
+++ myapp/models/index.js
@@ -5,6 +5,8 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB,dbConfig.USER,dbConfig.PASSWORD,{
     host : dbConfig.HOST,
     dialect : dbConfig.dialect,
+    logging : false,
+    benchmark : true,
     operatorsAliases : false,
     pool : {
         max : dbConfig.pool.max,
